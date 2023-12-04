97acf55cd8e526a894fad3e480484180ed5120a8
fayssaldevs <fayssal.elbouhamedy19@gmail.Com>
2023-12-04 01:59:26 +0100

    12/04

--- index.js
+++ index.js
@@ -202,3 +202,22 @@ hidseButton.addEventListener('click', () => {
 }
 
 window.addEventListener('scroll', scrollValue);
+
+ /* ******************* Count animation ********************* */
+
+ let valuedisplay = document.querySelectorAll(".number");
+ let interval = 1000;
+
+
+ valuedisplay.forEach((valuedisplays) =>{
+   let startvalue = 0;
+   let endvalue =parseInt(valuedisplays.getAttribute("data-val"));
+   let duration = Math.floor(interval /endvalue);
+   let counter =setInterval(function (){
+      startvalue += 10;
+      valuedisplays.textContent = startvalue ;
+      if ( startvalue == endvalue) {
+         clearInterval(counter);
+      }
+   }, duration);
+ })
